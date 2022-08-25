0758238fcb4c199a13a461877d7eb7cc77d96e31
nityanandojha <nityanandojha20@gmail.com>
2022-08-25 23:20:39 +0530

    added balance model

---
+++ controllers/balance.Controller.js
@@ -0,0 +1,17 @@
+const usersModel = require("../models/balancesmodel")
+
+module.exports.balance = function(req, res){
+    const {mobile} = req.body;
+    const balance = new usersModel({
+        name:req.body.name,
+        mobile:req.body.money
+    })
+
+    balance.save().then(data=>{
+        res.json(data);
+    })
+    .catch(error=>{
+        res.json(error)
+    })
+    
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const LoginManagement = require("../controllers/LoginManagement.controller");
+const BalanceManagement = require("../controllers/balance.Controller")
 
 router.route('/signup').post(LoginManagement.signup);
-
+router.route('/balance').post(BalanceManagement.balance);
 
 
 
